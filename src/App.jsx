85e2388cc38a915d1cfe6a89eb62fb98bc9bbb65
Majudsg <150301441+Majudsg@users.noreply.github.com>
2023-11-13 23:49:08 +0000

    tags

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ function App() {
     <>
       <div className="card">
         <h2>Rick Sanchez</h2>
-        <ul>
-          <li>Status: Vivo</li>
-          <li>Espécie: Humana</li>
-          <li>Origem: Terra C-137</li>
-        </ul>
+        <div className="tags">
+          <div className="tag">Status: Vivo</div>
+          <div className="tag">Espécie: Humana</div>
+          <div className="tag">Origem: Terra C-137</div>
+        </div>
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
       </div>
     </>
