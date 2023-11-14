e78b3a285ddf454cb796da63f97640c316d29ee9
Majudsg <150301441+Majudsg@users.noreply.github.com>
2023-11-14 23:14:55 +0000

    estilização lista de cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
 import './App.css'
-import Card from './components/Card/Card'
+import Card from './components/Card/Card';
 
 function App() {
+  const item1 = {
+    name: "Rick Sanchez",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    tags: ["Status: Vivo", "Espécie: Humana", "Origem: Terra C-137"]
+  }
+
+  const item2 = {
+    name: "Morty Smith",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    tags: ["Status: Vivo", "Espécie: Humana", "Origem: Terra C-137"]
+  }
+
+  const item3 = {
+    name: "Summer Smith",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+    tags: ["Status: Vivo", "Espécie: Humana", "Origem: Terra C-137"]
+  }
+
+  const items = [item1, item2, item3]
+
   return (
     <>
-      <div className="card">
-        <h2>Rick Sanchez</h2>
-        <div className="tags">
-          <div className="tag">Status: Vivo</div>
-          <div className="tag">Espécie: Humana</div>
-          <div className="tag">Origem: Terra C-137</div>
-        </div>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+    <div className="cards">
+      {items.map(function (element) {
+        return <Card item={element} />
+      })}
       </div>
-      <Card />
     </>
   )
 }
 
-export default App
+export default App;
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,3 +1,18 @@
-export default function Card() {
-    return<div>Card</div>
+import Tag from "../Tag/Tag"
+
+export default function Card(props) {
+  const item = props.item
+
+  const tags = item.tags || []
+
+
+  return <div className="card">
+    <h2>{item.name}</h2>
+    <div className="tags">
+      {tags.map(function (element) {
+        return <Tag text={element} />
+      })}
+    </div>
+    <img src={item.imageUrl} />
+  </div>
 }
