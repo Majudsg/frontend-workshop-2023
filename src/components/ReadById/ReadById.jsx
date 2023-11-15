1fa9eb530a8f715a1a4f7cb482e146ba7a8f9640
Majudsg <150301441+Majudsg@users.noreply.github.com>
2023-11-15 01:30:47 +0000

    To do's

--- src/components/ReadById/ReadById.jsx
+++ src/components/ReadById/ReadById.jsx
@@ -4,5 +4,9 @@ export default function ReadById() {
     const { id } = useParams()
     const apiUrl = `https://rickandmortyapi.com/api/character/${id}`
 
+    //Implementar o carregamento da API
+    //Guardar info recebida da API em um estado 
+    //Exibir essas informações no JSX
+
     return <div>ReadById: {id}</div>
 }
