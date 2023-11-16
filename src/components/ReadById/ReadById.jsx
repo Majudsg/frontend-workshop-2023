1d2121199d76378d3be150d7e19f396c542ab639
Majudsg <150301441+Majudsg@users.noreply.github.com>
2023-11-16 23:45:38 +0000

    carregar dados api

--- src/components/ReadById/ReadById.jsx
+++ src/components/ReadById/ReadById.jsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 export default function ReadById() {
     const { id } = useParams()
-    const apiUrl = `https://rickandmortyapi.com/api/character/${id}`
 
+    const [item, setItem] = useState({})
+
+    async function carregarDadosApi() {
+        const apiUrl = `https://rickandmortyapi.com/api/character/${id}`
+
+        const response = await fetch(apiUrl)
+    
+        const body = await response.json()
+
+        setItem(body)
+    
+    }
     //Implementar o carregamento da API
     //Guardar info recebida da API em um estado 
     //Exibir essas informações no JSX
 
-    return <div>ReadById: {id}</div>
+    useEffect(function() {
+        carregarDadosApi()
+      }, [])
+    
+
+    return <div>
+
+
+
+    </div>
 }
